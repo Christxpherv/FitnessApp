5235071c601897f2958bfab586f327bda4466b09
Christopher Valle <113216850+Christxpherv@users.noreply.github.com>
2022-12-31 09:17:31 -0600

    app

---
+++ Workout Planner/Workout_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_PlannerApp.swift
+//  Workout Planner
+//
+//  Created by Christopher Valle  on 12/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Workout_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
