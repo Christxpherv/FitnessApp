dcfa47f595a933a55838e4e09e715eff473e4d40
Christopher Valle <113216850+Christxpherv@users.noreply.github.com>
2023-01-03 16:02:30 -0600

    oof

--- Workout Planner/ContentView.swift
+++ Workout Planner/ContentView.swift
@@ -1,22 +1,37 @@
-//
-//  ContentView.swift
-//  Workout Planner
-//
-//  Created by Christopher Valle  on 12/30/22.
-//
-
 import SwiftUI
+import SceneKit
 
 struct ContentView: View {
-    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, world!")
+            }
+            .padding()
+            .tabItem {
+                Image(systemName: "1.circle")
+                Text("Home")
+            }
+            ProfileView()
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("Profile")
+                }
+            Text("Explore")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Explore")
+                }
         }
-        .padding()
+    }
+}
+
+struct ProfileView: View {
+    var body: some View {
+        Text("This is the profile view")
     }
 }
 
