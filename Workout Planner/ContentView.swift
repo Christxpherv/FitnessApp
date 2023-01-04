05637f52fb33a0e1253baad689f6f5cfd5d434c2
Christopher Valle <113216850+Christxpherv@users.noreply.github.com>
2023-01-04 12:01:35 -0600

    updating app

--- Workout Planner/ContentView.swift
+++ Workout Planner/ContentView.swift
@@ -10,20 +10,44 @@ struct ContentView: View {
                     .foregroundColor(.accentColor)
                 Text("Hello, world!")
             }
-            .padding()
             .tabItem {
-                Image(systemName: "1.circle")
+                Image(uiImage: UIImage(named: "home-home_symbol")!)
                 Text("Home")
+            }
+            Text("Exercises")
+                .tabItem {
+                    Image(uiImage: UIImage(named:"self_improvement")!)
+                    Text("Exercises")
+                }
+            Text("Progress")
+                .tabItem {
+                    Image(systemName: "chart.bar")
+                    Text("Progress")
+                }
+            VStack {
+                HStack {
+                    Button(action: {
+                    }) {
+                        Text("Following")
+                    }
+                    Button(action: {
+                    }) {
+                        Text("For You")
+                    }
+                    Button(action: {
+                    }) {
+                        Text("Trending")
+                    }
+                }
+            }
+            .tabItem {
+                Image(systemName: "magnifyingglass")
+                Text("Explore")
             }
             ProfileView()
                 .tabItem {
                     Image(systemName: "person.fill")
                     Text("Profile")
-                }
-            Text("Explore")
-                .tabItem {
-                    Image(systemName: "magnifyingglass")
-                    Text("Explore")
                 }
         }
     }
@@ -31,10 +55,13 @@ struct ContentView: View {
 
 struct ProfileView: View {
     var body: some View {
-        Text("This is the profile view")
+        List {
+            Text("Option 1")
+            Text("Option 2")
+            Text("Option 3")
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
---
+++ tmp.swift
@@ -0,0 +1,64 @@
+//import SwiftUI
+//import SceneKit (use to add 3D model to the homepage)
+//
+//struct ContentView: View {
+//    var body: some View {
+//        TabView {
+//            VStack {
+//                Image(systemName: "globe")
+//                    .imageScale(.large)
+//                    .foregroundColor(.accentColor)
+//                Text("Hello, world!")
+//            }
+//            .padding()
+//            .tabItem {
+//                Image(systemName: "1.circle")
+//                Text("Home")
+//            }
+//            ExercisesView()
+//                .tabItem {
+//                    Image(systemName: "rectangle.stack")
+//                    Text("Exercises")
+//                }
+//            ProgressView()
+//                .tabItem {
+//                    Image(systemName: "chart.bar")
+//                    Text("Progress")
+//                }
+//            Text("Explore")
+//                .tabItem {
+//                    Image(systemName: "magnifyingglass")
+//                    Text("Explore")
+//                }
+//            ProfileView()
+//                .tabItem {
+//                    Image(systemName: "person.fill")
+//                    Text("Profile")
+//                }
+//        }
+//    }
+//}
+//
+//struct ProfileView: View {
+//    var body: some View {
+//        Text("This is the profile view")
+//    }
+//}
+//
+//struct ExercisesView: View {
+//    var body: some View {
+//        Text("This is the exercises view")
+//    }
+//}
+//
+//struct ProgressView: View {
+//    var body: some View {
+//        Text("This is the progress view")
+//    }
+//    struct ContentView_Previews: PreviewProvider {
+//        static var previews: some View {
+//            ContentView()
+//        }
+//    }
+//}
+//
